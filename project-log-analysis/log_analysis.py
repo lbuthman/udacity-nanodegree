e14d5c949ef270f1f5ebe4d766d7a29cfab59c08
lbuthman <lbuthman@gmail.com>
2017-08-14 04:14:05 -0500

    init commit

---
+++ project-log-analysis/log_analysis.py
@@ -0,0 +1,56 @@
+import log_analysis_db
+
+print("\n\tHi! And welcome to the Easy News database analysis tool.")
+
+MENU = """
+\tPlease enter the number of the query you would like to perform.\n
+\t1 - Discover the three most popular articles of all time.
+\t2 - See all authors ranked in popularity, most popular on top.
+\t3 - Find the days with error requests 1% or more
+\t0 - quit\n
+\tEnter your choice here: """
+
+def print_articles(articles):
+    for article in articles:
+        print("\tArticle Title: {} -- Total Views: {}".format(
+            article[0], article[1]))
+
+
+def print_authors(authors):
+    for author in authors:
+        print("\tAuthor Name: {} -- Total Views: {}".format(
+            author[0], author[1]))
+
+
+def print_days(days):
+    for day in days:
+        print("\tDay: {}-{}-{} -- Error Responses: {}%".format(
+        int(day[0]), int(day[1]), int(day[2]), day[3]))
+
+
+while True:
+    query = input(MENU)
+
+    try:
+        query = int(query)
+    except:
+        print() # error message handled in else below
+
+    if query == 0:
+        print("\tHave a good day!\n")
+        break;
+
+    elif query == 1:
+        print("\tYou selected 1. Hang tight, looking now ...\n")
+        print_articles(log_analysis_db.get_top_three_articles())
+
+    elif query == 2:
+        print("\tYou selected 2. Uno momento while I look ...\n")
+        print_authors(log_analysis_db.get_top_authors())
+
+    elif query == 3:
+        print("\tYou selected 3. This shouldn't take long.\n")
+        print_days(log_analysis_db.get_top_error_days())
+
+    else:
+        print("\tSorry, I don't recognize that input ...\n")
---
+++ project-log-analysis/log_analysis_db.py
@@ -0,0 +1,40 @@
+import psycopg2
+
+DBNAME = 'news'
+
+def get_top_three_articles():
+    """"Return the three most popular articles of all time."""
+    db = psycopg2.connect(database=DBNAME)
+    cursor = db.cursor()
+    cursor.execute("""SELECT title, COUNT(*) AS total FROM log
+        JOIN articles ON log.path ILIKE '%' || articles.slug
+        WHERE status != '404 NOT FOUND' AND path != '/'
+        GROUP BY title ORDER BY total DESC LIMIT 3;""")
+    articles = cursor.fetchall()
+    db.close()
+    return articles
+
+
+def get_top_authors():
+    """"Return the most popular authors of all time."""
+    db = psycopg2.connect(database=DBNAME)
+    cursor = db.cursor()
+    cursor.execute("""SELECT name, COUNT(*) as total FROM log
+        JOIN author_article ON log.path ILIKE '%' || author_article.slug
+        GROUP BY name ORDER BY total DESC;""")
+    authors = cursor.fetchall()
+    db.close()
+    return authors
+
+
+def get_top_error_days():
+    """Returns days >= 1 Percent error requests."""
+    db = psycopg2.connect(database=DBNAME)
+    cursor = db.cursor()
+    cursor.execute("""SELECT date_part('month', day) as month,
+        date_part('day', day) as day, date_part('year', day) as year,
+        CAST (round(percentage, 2) AS real) as percentage
+        FROM error_percentage WHERE percentage >= 1;""")
+    days = cursor.fetchall()
+    db.close()
+    return days
